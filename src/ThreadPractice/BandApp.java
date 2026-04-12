af28e6d552d24b1520fcf317faed52d6879764c8
TeddiZen-X230 <2576903455@qq.com>
2026-04-12 20:29:48 +0800

    学会了多线程锁的思想

--- src/ThreadPractice/BandApp.java
+++ src/ThreadPractice/BandApp.java
@@ -1,5 +1,18 @@
 package ThreadPractice;
 
 public class BandApp {
+    public static void main(String[] args) {
+        BandCount bandCount = new BandCount("账户1", 10000);
+        Thread thread1 = new Thread(() -> {
+            System.out.println("线程1取款");
+            bandCount.getMount(10000);
+        });
+        Thread thread2 = new Thread(() -> {
+            System.out.println("线程2取款");
+            bandCount.getMount(10000);
+        });
+        thread1.start();
+        thread2.start();
+    }
 
 }
--- src/ThreadPractice/BandCount.java
+++ src/ThreadPractice/BandCount.java
@@ -1,19 +1,56 @@
 package ThreadPractice;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class BandCount {
      private String name;
      private int count;
+     private Lock lock = new ReentrantLock();
+     public BandCount(String name, int count) {
+        this.name = name;
+        this.count = count;
+     }
+     // 代码块同步
 
-     public void getMount(int count){
-          if (count >= getCount()){
-               System.out.println("剩余余额" + getCount() +"元");
+     // public void getMount(int count){
+     //      synchronized (this) {
+     //                if (this.count >= count){
+     //                System.out.println("剩余余额" + this.count +"元,可以取款");
+     //                this.count -= count;
+     //                System.out.println("取款成功,当前余额" + this.count +"元");
+     //           }else{
+     //                System.out.println("余额不足");
+     //           }
+     //      }
+     // }
+
+     // 方法同步
+     // public synchronized void getMount(int count){
+     //      if (this.count >= count){
+     //           System.out.println("剩余余额" + this.count +"元,可以取款");
+     //           this.count -= count;
+     //           System.out.println("取款成功,当前余额" + this.count +"元");
+     //      }else{
+     //           System.out.println("余额不足");
+     //      }
+     // }
+
+     // 手动lock同步
+     public  void getMount(int count){
+          lock.lock();
+          try {
+               if (this.count >= count){
+                    System.out.println("剩余余额" + this.count +"元,可以取款");
+                    this.count -= count;
+                    System.out.println("取款成功,当前余额" + this.count +"元");
+               }else{
+                    System.out.println("余额不足");
+               }
+          } finally {
+               lock.unlock();
           }
      }
 }
+
+     
