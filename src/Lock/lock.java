e123ca72d1f08543ebed352d52130aceac2a8a87
TeddiZen-X230 <2576903455@qq.com>
2026-05-04 23:54:45 +0800

    重新尝试死锁情况

--- src/Lock/lock.java
+++ src/Lock/lock.java
@@ -7,20 +7,40 @@ public class lock {
     public static void main(String[] args) {
         
         Thread thread1 = new Thread(() -> {
-            System.out.println("线程1开始");
-            synchronized (lock2) {
-                System.out.println("线程2锁");
+            System.out.println("线程一开始");
+            synchronized (lock1) {
+                System.out.println("线程一1锁开始");
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("线程一1锁结束");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("线程一2锁");
+                }
+                System.out.println("线程一1锁结束");
             }
-            System.out.println("线程1结束");
+            System.out.println("线程一结束");
         });
         Thread thread2 = new Thread(() -> {
-            System.out.println("线程2开始");
+            System.out.println("线程二开始");
             synchronized (lock1) {
-                System.out.println("线程1锁");
+                System.out.println("线程二1锁开始");
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("线程二1锁结束");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("线程二2锁");
+                }
+                System.out.println("线程二1锁结束");
             }
-            System.out.println("线程2结束");
+            System.out.println("线程二结束");
         });
-        
+        thread1.start();
         thread2.start();
     }
         
